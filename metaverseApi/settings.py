56dfdf605b65dab9d4d3c6b3449ee6af07022f65
kingmozzi <kingmozzi@gmail.com>
2022-11-02 15:52:22 +0900

    2022-11-02 debug false

--- metaverseApi/settings.py
+++ metaverseApi/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-fxeo%n%babaayi
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = False#bool(os.environ.get('DJANGO_DEBUG', False))
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
 
 ALLOWED_HOSTS = ['*']
